8105feb9341173dfbf84905564bed0a2753b9dcc
17Vector <131187562+17Vector@users.noreply.github.com>
2024-06-10 15:15:40 +0500

    Update GitHookController.php

--- app/Http/Controllers/GitHookController.php
+++ app/Http/Controllers/GitHookController.php
@@ -8,6 +8,18 @@ use Illuminate\Support\Facades\Log;
 class GitHookController extends Controller
 {
     private $isAction = false;
+    private $gitCommands = [
+        'git checkout main',
+        'git fetch --all',
+        'git reset --hard origin/main',
+        'git pull origin main',
+    ];
+    private $descripList = [
+        'Переключение на главную ветку',
+        'Получение сведений о текущих изменениях',
+        'Откат изменений до последнего коммита main',
+        'Обновление проекта с Git до последней версии'
+    ];
 
     public function gitAction(Request $request)
     {
@@ -24,14 +36,19 @@ class GitHookController extends Controller
 
         $this -> createLog($request);
         
-        try {
-            $this -> changeCode();
-            $this -> createLog($request);
+        try {  
+            foreach ($this->gitCommands as $key => $stage) {
+                Log::info("Идёт выполнение команды: $stage", ['description' => $this->descripList[$key]]);
+                $result = shell_exec($stage);
+                Log::info($result);
+            }
+            
+            $this -> isAction = false;
             return response()->json(['message' => 'Изменение кода прошло успешно.'], 200);
         }
         
         catch (\Exception $e) {
-            $this -> createLog($request);
+            $this -> isAction = false;
             return response()->json(['message' => 'Произошла ошибка при изменении кода.'], 500);
         }
     }
@@ -42,27 +59,5 @@ class GitHookController extends Controller
             'ip' => $request -> ip(),
         ];
         Log::info('Изменение кода...', $newLog);
-    }
-
-    private function changeCode() {
-        $gitCommands = [
-            'git checkout main',
-            'git fetch --all',
-            'git reset --hard origin/main',
-            'git pull origin main',
-        ];
-
-        $descripList = [
-            'Переключение на главную ветку',
-            'Получение сведений о текущих изменениях',
-            'Откат изменений до последнего коммита main',
-            'Обновление проекта с Git до последней версии'
-        ];
-
-        foreach ($gitCommands as $key => $stage) {
-            Log::info("Идёт выполнение команды: $stage", ['description' => $descripList[$key]]);
-            $result = shell_exec($stage);
-            Log::info($result);
-        }
     }
 }
