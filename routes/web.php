f70257166488c668af9cc422df328691cdd2b79c
17Vector <131187562+17Vector@users.noreply.github.com>
2024-06-10 13:34:15 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GitWebhookController;
+use App\Http\Controllers\GitHookController;
 
 /*
 |--------------------------------------------------------------------------
